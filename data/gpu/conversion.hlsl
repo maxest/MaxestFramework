069cbac31b8728a83a4d6fd8f4de9dc148753f5c
Wojciech Sterna <xwost@saxobank.com>
2018-10-30 11:56:39 +0100

    - more color conversion standard functions (rgb <=> yuv), both in c++ and hlsl

--- data/gpu/conversion.hlsl
+++ data/gpu/conversion.hlsl
@@ -28,7 +28,8 @@ float3 RGBToLumaChroma(float3 value)
 	transform[2][2] = -0.081312f;
 	
 	value = mul(transform, value);
-	value.yz += 0.5f;
+	value.y += 0.5f;
+	value.z += 0.5f;
 	
 	return value;
 }
@@ -47,10 +48,139 @@ float3 LumaChromaToRGB(float3 value)
 	transform[2][1] = 1.772f;
 	transform[2][2] = 0.0f;
 
-	value.yz -= 0.5f;
+	value.y -= 0.5f;
+	value.z -= 0.5f;
+
+	return mul(transform, value);
+}
+
+
+float3 RGBToLumaChroma_BT601(float3 value)
+{
+	float3x3 transform;
+	transform[0][0] = 0.256788254f;
+	transform[0][1] = 0.504129469f;
+	transform[0][2] = 0.0979058892f;
+	transform[1][0] = -0.144914359f;
+	transform[1][1] = -0.284497440f;
+	transform[1][2] = 0.429411799f;
+	transform[2][0] = 0.429411799f;
+	transform[2][1] = -0.359578788f;
+	transform[2][2] = -0.0698330179f;
+
 	value = mul(transform, value);
+	value.x += 16.0f / 255.0f;
+	value.y += 128.0f / 255.0f;
+	value.z += 128.0f / 255.0f;
 
 	return value;
+}
+
+
+float3 LumaChromaToRGB_BT601(float3 value)
+{
+	float3x3 transform;
+	transform[0][0] = 1.16438353f;
+	transform[0][1] = 0.0f;
+	transform[0][2] = 1.63246572f;
+	transform[1][0] = 1.16438353f;
+	transform[1][1] = -0.400706619f;
+	transform[1][2] = -0.831528485f;
+	transform[2][0] = 1.16438353f;
+	transform[2][1] = 2.06328773f;
+	transform[2][2] = 0.0f;
+
+	value.x -= 16.0f / 255.0f;
+	value.y -= 128.0f / 255.0f;
+	value.z -= 128.0f / 255.0f;
+
+	return mul(transform, value);
+}
+
+
+float3 RGBToLumaChroma_BT709(float3 value)
+{
+	float3x3 transform;
+	transform[0][0] = 0.182585895f;
+	transform[0][1] = 0.614230633f;
+	transform[0][2] = 0.0620070547f;
+	transform[1][0] = -0.0983972251f;
+	transform[1][1] = -0.331014544f;
+	transform[1][2] = 0.429411799f;
+	transform[2][0] = 0.429411799f;
+	transform[2][1] = -0.390037209f;
+	transform[2][2] = -0.0393745564f;
+
+	value = mul(transform, value);
+	value.x += 16.0f / 255.0f;
+	value.y += 128.0f / 255.0f;
+	value.z += 128.0f / 255.0f;
+
+	return value;
+}
+
+
+float3 LumaChromaToRGB_BT709(float3 value)
+{
+	float3x3 transform;
+	transform[0][0] = 1.16438353f;
+	transform[0][1] = 0.0f;
+	transform[0][2] = 1.83367121f;
+	transform[1][0] = 1.16438353f;
+	transform[1][1] = -0.218117282f;
+	transform[1][2] = -0.545076191f;
+	transform[2][0] = 1.16438353f;
+	transform[2][1] = 2.16063023f;
+	transform[2][2] = 0.0f;
+
+	value.x -= 16.0f / 255.0f;
+	value.y -= 128.0f / 255.0f;
+	value.z -= 128.0f / 255.0f;
+
+	return mul(transform, value);
+}
+
+
+float3 RGBToLumaChroma_BT2020(float3 value)
+{
+	float3x3 transform;
+	transform[0][0] = 0.224735081f;
+	transform[0][1] = 0.580016613f;
+	transform[0][2] = 0.0507300682f;
+	transform[1][0] = -0.119450986f;
+	transform[1][1] = -0.308289915f;
+	transform[1][2] = 0.427740902f;
+	transform[2][0] = 0.427740872f;
+	transform[2][1] = -0.393338263f;
+	transform[2][2] = -0.0344025977f;
+
+	value = mul(transform, value);
+	value.x += 16.0f / 255.0f;
+	value.y += 128.0f / 255.0f;
+	value.z += 128.0f / 255.0f;
+
+	return value;
+}
+
+
+float3 LumaChromaToRGB_BT2020(float3 value)
+{
+	float3x3 transform;
+	transform[0][0] = 1.16893196f;
+	transform[0][1] = 0.0f;
+	transform[0][2] = 1.72370708f;
+	transform[1][0] = 1.16893196f;
+	transform[1][1] = -0.192351416f;
+	transform[1][2] = -0.667873025f;
+	transform[2][0] = 1.16893196f;
+	transform[2][1] = 2.19922853f;
+	transform[2][2] = 0.0f;
+
+	value.x -= 16.0f / 255.0f;
+	value.y -= 128.0f / 255.0f;
+	value.z -= 128.0f / 255.0f;
+
+	return mul(transform, value);
 }
 
 
--- src/math/color.h
+++ src/math/color.h
@@ -13,8 +13,14 @@ namespace NMaxestFramework { namespace NMath
 
 	uint32 Vector4ToUInt32(const SVector4& v);
 
-	SVector3 RGBToLumaChroma(const SVector3& rgb);
-	SVector3 LumaChromaToRGB(SVector3 lumaChroma);
+	SVector3 RGBToLumaChroma(SVector3 value);
+	SVector3 LumaChromaToRGB(SVector3 value);
+	SVector3 RGBToLumaChroma_BT601(SVector3 value);
+	SVector3 LumaChromaToRGB_BT601(SVector3 value);
+	SVector3 RGBToLumaChroma_BT709(SVector3 value);
+	SVector3 LumaChromaToRGB_BT709(SVector3 value);
+	SVector3 RGBToLumaChroma_BT2020(SVector3 value);
+	SVector3 LumaChromaToRGB_BT2020(SVector3 value);
 
 	//
 
@@ -51,45 +57,165 @@ namespace NMaxestFramework { namespace NMath
 			(uint8)(255.0f * Saturate(v.w)));
 	}
 
-	inline SVector3 RGBToLumaChroma(const SVector3& rgb)
+	inline SVector3 RGBToLumaChroma(SVector3 value)
 	{
-		SMatrix matrix;
+		SMatrix transform;
+		transform.m[0][0] = 0.299f;
+		transform.m[0][1] = 0.587f;
+		transform.m[0][2] = 0.114f;
+		transform.m[1][0] = -0.168736f;
+		transform.m[1][1] = -0.331264f;
+		transform.m[1][2] = 0.5f;
+		transform.m[2][0] = 0.5f;
+		transform.m[2][1] = -0.418688f;
+		transform.m[2][2] = -0.081312f;
 
-		matrix.m[0][0] = 0.299f;
-		matrix.m[0][1] = 0.587f;
-		matrix.m[0][2] = 0.114f;
-		matrix.m[1][0] = -0.168736f;
-		matrix.m[1][1] = -0.331264f;
-		matrix.m[1][2] = 0.5f;
-		matrix.m[2][0] = 0.5f;
-		matrix.m[2][1] = -0.418688f;
-		matrix.m[2][2] = -0.081312f;
+		value = transform * value;
+		value.y += 0.5f;
+		value.z += 0.5f;
 
-		SVector3 result = matrix * rgb;
-		result.y += 0.5f;
-		result.z += 0.5f;
-
-		return result;
+		return value;
 	}
 
-
-	inline SVector3 LumaChromaToRGB(SVector3 lumaChroma)
+	inline SVector3 LumaChromaToRGB(SVector3 value)
 	{
-		lumaChroma.y -= 0.5f;
-		lumaChroma.z -= 0.5f;
+		SMatrix transform;
+		transform.m[0][0] = 1.0f;
+		transform.m[0][1] = 0.0f;
+		transform.m[0][2] = 1.402f;
+		transform.m[1][0] = 1.0f;
+		transform.m[1][1] = -0.344136f;
+		transform.m[1][2] = -0.714136f;
+		transform.m[2][0] = 1.0f;
+		transform.m[2][1] = 1.772f;
+		transform.m[2][2] = 0.0f;
 
-		SMatrix matrix;
+		value.y -= 0.5f;
+		value.z -= 0.5f;
 
-		matrix.m[0][0] = 1.0f;
-		matrix.m[0][1] = 0.0f;
-		matrix.m[0][2] = 1.402f;
-		matrix.m[1][0] = 1.0f;
-		matrix.m[1][1] = -0.344136f;
-		matrix.m[1][2] = -0.714136f;
-		matrix.m[2][0] = 1.0f;
-		matrix.m[2][1] = 1.772f;
-		matrix.m[2][2] = 0.0f;
+		return transform * value;
+	}
 
-		return matrix * lumaChroma;
+	inline SVector3 RGBToLumaChroma_BT601(SVector3 value)
+	{
+		SMatrix transform;
+		transform.m[0][0] = 0.256788254f;
+		transform.m[0][1] = 0.504129469f;
+		transform.m[0][2] = 0.0979058892f;
+		transform.m[1][0] = -0.144914359f;
+		transform.m[1][1] = -0.284497440f;
+		transform.m[1][2] = 0.429411799f;
+		transform.m[2][0] = 0.429411799f;
+		transform.m[2][1] = -0.359578788f;
+		transform.m[2][2] = -0.0698330179f;
+
+		value = transform * value;
+		value.x += 16.0f / 255.0f;
+		value.y += 128.0f / 255.0f;
+		value.z += 128.0f / 255.0f;
+
+		return value;
+	}
+
+	inline SVector3 LumaChromaToRGB_BT601(SVector3 value)
+	{
+		SMatrix transform;
+		transform.m[0][0] = 1.16438353f;
+		transform.m[0][1] = 0.0f;
+		transform.m[0][2] = 1.63246572f;
+		transform.m[1][0] = 1.16438353f;
+		transform.m[1][1] = -0.400706619f;
+		transform.m[1][2] = -0.831528485f;
+		transform.m[2][0] = 1.16438353f;
+		transform.m[2][1] = 2.06328773f;
+		transform.m[2][2] = 0.0f;
+
+		value.x -= 16.0f / 255.0f;
+		value.y -= 128.0f / 255.0f;
+		value.z -= 128.0f / 255.0f;
+
+		return transform * value;
+	}
+
+	inline SVector3 RGBToLumaChroma_BT709(SVector3 value)
+	{
+		SMatrix transform;
+		transform.m[0][0] = 0.182585895f;
+		transform.m[0][1] = 0.614230633f;
+		transform.m[0][2] = 0.0620070547f;
+		transform.m[1][0] = -0.0983972251f;
+		transform.m[1][1] = -0.331014544f;
+		transform.m[1][2] = 0.429411799f;
+		transform.m[2][0] = 0.429411799f;
+		transform.m[2][1] = -0.390037209f;
+		transform.m[2][2] = -0.0393745564f;
+
+		value = transform * value;
+		value.x += 16.0f / 255.0f;
+		value.y += 128.0f / 255.0f;
+		value.z += 128.0f / 255.0f;
+
+		return value;
+	}
+
+	inline SVector3 LumaChromaToRGB_BT709(SVector3 value)
+	{
+		SMatrix transform;
+		transform.m[0][0] = 1.16438353f;
+		transform.m[0][1] = 0.0f;
+		transform.m[0][2] = 1.83367121f;
+		transform.m[1][0] = 1.16438353f;
+		transform.m[1][1] = -0.218117282f;
+		transform.m[1][2] = -0.545076191f;
+		transform.m[2][0] = 1.16438353f;
+		transform.m[2][1] = 2.16063023f;
+		transform.m[2][2] = 0.0f;
+
+		value.x -= 16.0f / 255.0f;
+		value.y -= 128.0f / 255.0f;
+		value.z -= 128.0f / 255.0f;
+
+		return transform * value;
+	}
+
+	inline SVector3 RGBToLumaChroma_BT2020(SVector3 value)
+	{
+		SMatrix transform;
+		transform.m[0][0] = 0.224735081f;
+		transform.m[0][1] = 0.580016613f;
+		transform.m[0][2] = 0.0507300682f;
+		transform.m[1][0] = -0.119450986f;
+		transform.m[1][1] = -0.308289915f;
+		transform.m[1][2] = 0.427740902f;
+		transform.m[2][0] = 0.427740872f;
+		transform.m[2][1] = -0.393338263f;
+		transform.m[2][2] = -0.0344025977f;
+
+		value = transform * value;
+		value.x += 16.0f / 255.0f;
+		value.y += 128.0f / 255.0f;
+		value.z += 128.0f / 255.0f;
+
+		return value;
+	}
+
+	inline SVector3 LumaChromaToRGB_BT2020(SVector3 value)
+	{
+		SMatrix transform;
+		transform.m[0][0] = 1.16893196f;
+		transform.m[0][1] = 0.0f;
+		transform.m[0][2] = 1.72370708f;
+		transform.m[1][0] = 1.16893196f;
+		transform.m[1][1] = -0.192351416f;
+		transform.m[1][2] = -0.667873025f;
+		transform.m[2][0] = 1.16893196f;
+		transform.m[2][1] = 2.19922853f;
+		transform.m[2][2] = 0.0f;
+
+		value.x -= 16.0f / 255.0f;
+		value.y -= 128.0f / 255.0f;
+		value.z -= 128.0f / 255.0f;
+
+		return transform * value;
 	}
 } }
