d117880d37ef89a85cde84bf39569b4f34f53bb7
Wojciech Sterna <xwost@saxobank.com>
2018-11-05 18:55:04 +0100

    - better assert

--- src/essentials/assert.h
+++ src/essentials/assert.h
@@ -1,20 +1,25 @@
 #pragma once
 
 
+#include <cstdio>
 #ifndef MAXEST_FRAMEWORK_WINDOWS
 	#include <signal.h>
 #endif
 
-
-#define MF_ASSERT(condition)	NMaxestFramework::NEssentials::Assert(condition)
+#define MF_ASSERT(condition)	NMaxestFramework::NEssentials::Assert(condition, __FILE__, __LINE__)
 
 
 namespace NMaxestFramework { namespace NEssentials
 {
-	inline void Assert(bool condition)
-	{	
+	inline void Assert(bool condition, const char* fileName, int line)
+	{
 		if (!(condition))
 		{
+			FILE* file;
+			fopen_s(&file, "assert.txt", "w");
+			fprintf(file, "%s  %d\n", fileName, line);
+			fclose(file);
+
 		#ifdef MAXEST_FRAMEWORK_WINDOWS
 			__debugbreak();
 		#else
